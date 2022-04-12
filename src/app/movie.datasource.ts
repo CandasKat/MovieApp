303f3ec0b873c7b68e1e36c9d424928c179399cf
CandasKat <hrisohest@gmail.com>
2022-04-12 11:35:18 +0200

    12-Dashboard ekrani ve film resimleri eklenerek giris ekrani olusturuldu

--- src/app/movie.datasource.ts
+++ src/app/movie.datasource.ts
@@ -1,10 +1,13 @@
 import { Movie } from "./movie";
 
 export const Movies: Movie[] = [
-    {id:1, name:"movie 1"},
-    {id:2, name:"movie 2"},
-    {id:3, name:"movie 3"},
-    {id:4, name:"movie 4"},
-    {id:5, name:"movie 5"},
-    {id:6, name:"movie 6"}
+    {id:1, name:"Good Will Hunting",description:"guzel film",imageUrl:"1.jpg"},
+    {id:2, name:"Intouchables",description:"guzel film",imageUrl:"2.jpg"},
+    {id:3, name:"The Shawshank Redemption",description:"guzel film",imageUrl:"3.jpg"},
+    {id:4, name:"Sherlock Holmes",description:"guzel film",imageUrl:"4.jpg"},
+    {id:5, name:"Braveheart",description:"guzel film",imageUrl:"5.jpg"},
+    {id:6, name:"The Lord of The Rings",description:"guzel film",imageUrl:"6.jpg"},
+    {id:7, name:"Spirited Away",description:"guzel film",imageUrl:"7.jpg"},
+    {id:8, name:"Leon",description:"guzel film",imageUrl:"8.jpg"},
+    {id:9, name:"V for Vandetta",description:"guzel film",imageUrl:"9.jpg"}
 ];
