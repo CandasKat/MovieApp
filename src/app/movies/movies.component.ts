ada059b4d901010793136ac903b689a89a769cbc
CandasKat <hrisohest@gmail.com>
2022-04-12 08:58:56 +0200

    8-Service componenti eklendi ve movie listesi buradan alindi

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { Movie } from "../movie";
-import { Movies } from "../movie.datasource";
+import { MovieService } from "../movie.service";
+
 
 @Component({
     selector: 'movies',
@@ -9,10 +10,22 @@ import { Movies } from "../movie.datasource";
 export class MoviesComponent{
     title= 'Movie List';
 
-    movies=Movies;
+    movies!: Movie[];
     selectedMovie!: Movie;
+
+    constructor(private movieService:MovieService){}
+
+    ngOnInit(): void {
+        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        //Add 'implements OnInit' to the class.
+        this.getMovies();
+    }
+
     onSelect(movie:Movie): void {
         this.selectedMovie = movie;
+    }
+    getMovies():void{
+        this.movies=this.movieService.getMovies();
     }
     
 }
